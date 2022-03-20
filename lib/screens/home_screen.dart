bd60058047a787fcdc524c8cae6f038bd753acce
José Hernández <Jose_Hernandez_c@outlook.com>
2022-03-20 11:07:39 -0600

    Implementado el scanner de Qr

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,115 +22,21 @@ class HomePage extends StatelessWidget {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Row(
-                  children: [
-                    Container(
-                      //color: Colors.grey,
-                      child: Text(
-                        "35",
-                        style: TextStyle(
-                          color: MyColors.white,
-                          fontSize: 22,
-                          fontFamily: GoogleFonts.lato().fontFamily,
-                          decoration: TextDecoration.none,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(left: 20),
-                      //color: Colors.amber,
-                      child: Image.asset(
-                        "assets/icons/trophy.png",
-                        scale: 1.5,
-                      ),
-                    )
-                  ],
+                IconProgress(
+                  value: '35',
+                  iconPath: 'assets/icons/trophy.png',
                 ),
-                Row(
-                  children: [
-                    Container(
-                      //color: Colors.grey,
-                      child: Text(
-                        "1,507",
-                        style: TextStyle(
-                          color: MyColors.white,
-                          fontSize: 22,
-                          fontFamily: GoogleFonts.lato().fontFamily,
-                          decoration: TextDecoration.none,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(left: 20),
-                      //color: Colors.amber,
-                      child: Image.asset(
-                        "assets/icons/bag.png",
-                        scale: 1.5,
-                      ),
-                    )
-                  ],
-                ),
+                IconProgress(
+                  value: '1,507',
+                  iconPath: 'assets/icons/bag.png',
+                )
               ],
             ),
           ),
           /*Nivel y progreso*/
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.grey.shade600.withAlpha(180),
-                borderRadius: BorderRadius.circular(10)),
-            width: size.width * 0.8,
-            height: 120,
-            margin: EdgeInsets.all(10),
-            child: Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: 10),
-                  //color: Colors.orange,
-                  child: Text(
-                    "Nivel 21",
-                    style: TextStyle(
-                        fontSize: 22,
-                        fontFamily: GoogleFonts.lato().fontFamily,
-                        color: MyColors.white,
-                        decoration: TextDecoration.none),
-                  ),
-                ),
-                Container(
-                  //color: Colors.purple,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 10),
-                        child: Image.asset(
-                          "assets/icons/passport.png",
-                          scale: 1.5,
-                        ),
-                      ),
-                      for (var i = 0; i < 6; i++)
-                        Container(
-                          color: MyColors.white,
-                          width: 5,
-                          height: 30,
-                        ),
-                      for (var i = 0; i < 4; i++)
-                        Container(
-                          color: Colors.grey.shade900,
-                          width: 5,
-                          height: 30,
-                        ),
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 10),
-                        child: Image.asset(
-                          "assets/icons/suitcase.png",
-                          scale: 1.5,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+          LevelProgressCard(
+            size: size,
+            lv: 28,
           ),
           /*Rango*/
           Container(
@@ -166,6 +72,120 @@ class HomePage extends StatelessWidget {
             margin: EdgeInsets.all(20),
             height: size.height * 0.25,
             child: Image.asset('assets/images/ajolote.png'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class IconProgress extends StatelessWidget {
+  String value;
+  String iconPath;
+
+  IconProgress({
+    Key? key,
+    required this.value,
+    required this.iconPath,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          //color: Colors.grey,
+          child: Text(
+            value,
+            style: TextStyle(
+              color: MyColors.white,
+              fontSize: 22,
+              fontFamily: GoogleFonts.lato().fontFamily,
+              decoration: TextDecoration.none,
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(left: 20),
+          //color: Colors.amber,
+          child: Image.asset(
+            iconPath,
+            scale: 1.5,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class LevelProgressCard extends StatelessWidget {
+  int lv;
+  LevelProgressCard({
+    Key? key,
+    required this.size,
+    required this.lv,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.grey.shade700.withAlpha(200),
+          borderRadius: BorderRadius.circular(50)),
+      width: size.width * 0.8,
+      height: 120,
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            //color: Colors.orange,
+            child: Text(
+              "Nivel $lv",
+              style: TextStyle(
+                  fontSize: 22,
+                  fontFamily: GoogleFonts.lato().fontFamily,
+                  color: MyColors.white,
+                  decoration: TextDecoration.none),
+            ),
+          ),
+          Container(
+            //color: Colors.purple,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  child: Image.asset(
+                    "assets/icons/passport.png",
+                    scale: 1.5,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      color: MyColors.white,
+                      width: 60,
+                      height: 15,
+                    ),
+                    Container(
+                      color: Colors.grey.shade900,
+                      width: 40,
+                      height: 15,
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  child: Image.asset(
+                    "assets/icons/suitcase.png",
+                    scale: 1.5,
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
