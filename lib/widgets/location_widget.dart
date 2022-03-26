c82225f7d655d25b28ef6b36274103e3916ffaf6
dannielgloria <danniel.gloria@gmail.com>
2022-03-25 18:35:21 -0600

    final changes to places

--- lib/widgets/location_widget.dart
+++ lib/widgets/location_widget.dart
@@ -26,26 +26,28 @@ class _LocationWidgetState extends State<LocationWidget> {
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 4),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          AnimatedPositioned(
-            duration: const Duration(milliseconds: 500),
-            bottom: isExpanded ? 40 : 100,
-            width: isExpanded ? size.width * 0.78 : size.width * 0.7,
-            height: isExpanded ? size.height * 0.6 : size.height * 0.5,
-            child: ExpandedContentLoc(location: widget.location),
-          ),
-          AnimatedPositioned(
-            duration: Duration(milliseconds: 500),
-            bottom: isExpanded ? 150 : 100,
-            child: GestureDetector(
-              onPanUpdate: onPanUpdate,
-              onTap: openDetailPage,
-              child: ImageWidget(location: widget.location),
+      child: Container(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            AnimatedPositioned(
+              duration: const Duration(milliseconds: 500),
+              bottom: isExpanded ? 40 : 100,
+              width: isExpanded ? size.width * 0.78 : size.width * 0.7,
+              height: isExpanded ? size.height * 0.6 : size.height * 0.5,
+              child: ExpandedContentLoc(location: widget.location),
             ),
-          ),
-        ],
+            AnimatedPositioned(
+              duration: Duration(milliseconds: 500),
+              bottom: isExpanded ? 150 : 100,
+              child: GestureDetector(
+                onPanUpdate: onPanUpdate,
+                onTap: openDetailPage,
+                child: ImageWidget(location: widget.location),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
